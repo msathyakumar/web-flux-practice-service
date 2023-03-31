0c4e967aed23898fb566383878c4fb1755c40e79
M Sathya Kumar <56182934+Sathyakumar-M@users.noreply.github.com>
2023-03-31 16:57:21 +0530

    Initial commit

---
+++ src/main/java/com/client/webfluxtest/WebFluxPracticeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.client.webfluxtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxPracticeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFluxPracticeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/client/webfluxtest/WebFluxPracticeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.client.webfluxtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFluxPracticeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
